dec3820e6b43a61789773ef1feede32afe2bfb4b
Andre Alves <andrecambridge@gmail.com>
2025-11-20 08:50:11 -0300

    feat: add pipeline build steps

--- infra/modules/ecs-fargate/2-ecs-task-definition.tf
+++ infra/modules/ecs-fargate/2-ecs-task-definition.tf
@@ -6,20 +6,5 @@ resource "aws_ecs_task_definition" "this" {
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.task_execution.arn
   task_role_arn            = aws_iam_role.task_role.arn
-  container_definitions = jsonencode([
-    {
-      name      = "first"
-      image     = "service-first"
-      cpu       = 10
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
-        }
-      ]
-      readonlyRootFilesystem = true
-    }
-  ])
+  container_definitions    = file("./taskdef.json")
 }
